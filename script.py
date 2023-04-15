98bb8ae5245f5f24f2fe1d4405bccba6002e2e8d
Anuu <110338926+Manishgupta64047@users.noreply.github.com>
2023-04-15 09:39:44 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,23 +1,19 @@
 class Script(object):
 
-    START_MSG = """<b>Hey 👋 {},
+    START_MSG = """<b>Hello Dear {} 😳,
 
- 𝐼 𝑎𝑚 𝑢𝑛𝑙𝑖𝑚𝑖𝑡𝑒𝑑 𝑚𝑎𝑛𝑢𝑎𝑙 𝑓𝑖𝑙𝑡𝑒𝑟 𝑏𝑜𝑡 𝑤𝑖𝑡ℎ 𝑎𝑢𝑡𝑜 𝑑𝑒𝑙𝑒𝑡𝑒 𝑠𝑢𝑝𝑝𝑜𝑟𝑡 𝑡𝑜 𝑎𝑣𝑜𝑖𝑑 𝑐𝑜𝑝𝑦𝑟𝑖𝑔ℎ𝑡 
+ 𝐼 𝑎𝑚 𝑢𝑛𝑙𝑖𝑚𝑖𝑡𝑒𝑑 𝑚𝑎𝑛𝑢𝑎𝑙 𝑓𝑖𝑙𝑡𝑒𝑟 𝑏𝑜𝑡 𝑤𝑖𝑡ℎ 𝑎𝑢𝑡𝑜 𝑑𝑒𝑙𝑒𝑡𝑒 𝑠𝑢𝑝𝑝𝑜𝑟𝑡 𝑡𝑜 𝑎𝑣𝑜𝑖𝑑 𝑐𝑜𝑝𝑦𝑟𝑖𝑔ℎ𝑡!
  
- 𝑖 𝑎𝑚  𝑝𝑒𝑟𝑠𝑜𝑛𝑎𝑙 𝐵𝑜𝑡 𝑛𝑜𝑡 𝑝𝑢𝑏𝑙𝑖𝑐 
-
-𝐼𝑓 𝑢 𝑤𝑎𝑛𝑡 𝑡ℎ𝑖𝑠 𝑡𝑦𝑝𝑒 𝑜𝑓 𝑏𝑜𝑡 𝑚𝑒𝑠𝑠𝑎𝑔𝑒𝑠 𝑚𝑦 𝑂𝑤𝑛𝑒𝑟 ℎ𝑒 𝑤𝑖𝑙𝑙 𝑔𝑖𝑣𝑒 𝑢 𝑓𝑢𝑙𝑙 𝑟𝑒𝑝𝑜 𝑏𝑢𝑡 𝑝𝑎𝑖𝑑 @Pankajpandiyar
-
-
-
-MADE BY @pankajpandiyar
+ 𝑖 𝑎𝑚 𝑎𝑛 𝑎𝑢𝑡𝑜 𝑝𝑢𝑏𝑙𝑖𝑐 𝐵𝑜𝑡!
 
 </b>
 """
 
 
     HELP_MSG = """
-<i> This Bot is only made for parsonal use :)</i>
+<i> This Bot is developed for telegram admins who have access to their supergroups </i>
+
+𝐼𝑓 𝑢 𝑤𝑎𝑛𝑡 𝑡ℎ𝑖𝑠 𝑡𝑦𝑝𝑒 𝑜𝑓 𝑏𝑜𝑡 𝑚𝑒𝑠𝑠𝑎𝑔𝑒𝑠 𝑚𝑦 𝑂𝑤𝑛𝑒𝑟 ℎ𝑒 𝑤𝑖𝑙𝑙 𝑔𝑖𝑣𝑒 𝑢 𝑓𝑢𝑙𝑙 𝑠𝑢𝑝𝑝𝑜𝑟𝑡!
 
 
 <b>Basic Commands;</b>
@@ -43,28 +39,25 @@ MADE BY @pankajpandiyar
 <code>/addgroup groupid</code>  -  Connect your group to my PM. You can also simply use,
 <code>/addgroup</code> in groups.
 
-
-
-
 <b>Extras</b>
-
-
 
 /id  -  Shows ID information
 
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
-
 <</b>
 """
 
+    ABOUT_MSG = """⭕️<b>My Name : Auto Post Filter Bot</b>
 
-    ABOUT_MSG = """⭕️<b>My Name : Filter Bot</b>
+👨‍💻<b>Developer:</b> <i> <a href='https://t.me/sigma_male_007'> <u>Dev <u></a>   </i>
 
-⭕️<b>Creater :</b> @pankajpandiyar   
+🧾<b>Language :</b><i> <a href='https://www.python.org/'><u>Python V3 </u></a></i>
 
-⭕️<b>Language :</b> <code>Python3</code>
+📖<b>Library :</b><i> <a href='https://docs.pyrogram.org/'><u>Pyrogram 1.0.7</u></a> </i>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+🔖<b>Used In: </b><i><a href='https://t.me/blackest_harbour'><u>BLACK MARKET</u></a></i>
+
+📡<b>Server:</b> <i><a href='https://heroku.com/'></a></i>
 
 """
